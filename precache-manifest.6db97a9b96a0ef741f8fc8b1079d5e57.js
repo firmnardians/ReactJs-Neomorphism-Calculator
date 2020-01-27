7c0641c8e1d8aaccc2d74a760587679d6e146e04
Ade Firman Ardiansyah <41628497+firmnardians@users.noreply.github.com>
2020-01-28 00:23:58 +0700

    React JS bos
    asik asik joz

---
+++ precache-manifest.6db97a9b96a0ef741f8fc8b1079d5e57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86be8cac7e87114134ec3160e28ce684",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b530e1714c09f6fa0f48",
+    "url": "/static/css/main.6c291e43.chunk.css"
+  },
+  {
+    "revision": "d4dd674a20699183b2a2",
+    "url": "/static/js/2.3d2b5854.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3d2b5854.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b530e1714c09f6fa0f48",
+    "url": "/static/js/main.3ff917bc.chunk.js"
+  },
+  {
+    "revision": "f60caca5cbe3cb44115a",
+    "url": "/static/js/runtime-main.29cc2f5b.js"
+  }
+]);
